e8ef24ac9073de8f878deab0f0fca676e5150872
shirleen <shirleenoey@gmail.com>
2023-05-04 16:54:13 +0700

    connecting profile to backend & db

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,23 @@ const handleProfileGet = (req, res, db) => {
 		.catch(err => res.status(400).json('error getting users'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+	const { id } = req.params;
+	const { name, age, pet } = req.body.formInput;
+	db('users')
+		.where({id: id})
+		.update({name: name})
+		.then(resp => {
+			if(resp) {
+				res.json('success')
+			} else {
+				res.status(400).json('unable to update')
+			}
+		})
+		.catch(err => res.status(400).json('error updating user'))
+}
+
 module.exports = {
-	handleProfileGet
+	handleProfileGet,
+	handleProfileUpdate
 }
--- server.js
+++ server.js
@@ -27,9 +27,10 @@ app.use(cors());
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => res.send('server is live'));
-app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) });
+app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
